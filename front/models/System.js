33d9bf7ad138dbc9978af54844ec16961fecbec0
caiquekola <caiquekola@hotmail.com>
2025-01-22 22:23:05 -0300

    feat: add functions in sfs

--- front/models/System.js
+++ front/models/System.js
@@ -2,12 +2,13 @@ import { Directory } from './Directory.js';
 
 export default class System{
     constructor(){
-        this.root = new Directory("C:", null);
+        this.root = new Directory("C:/",null);
         this.freeSpace = 0;
         this.totalSpace = 0;
         this.commandHistory = [];
     }
     
+
     getCommandHistory(){
         return this.commandHistory;
     }
--- front/services/SystemFilesService.js
+++ front/services/SystemFilesService.js
@@ -2,17 +2,32 @@ import System from '../models/System.js';
 
 class FileSystemService{
     constructor() {
-        this.root = new System("C:", null);
+        this.root = new System();
+        this.rootDir = "C:/";
+        this.currentDir = "C:/";
     }
 
-    // Função para dividir o caminho
+    //salva o último comando
+    addCommandHistory(command){
+        this.root.commandHistory.push(command);
+    }
+
+    // Função para dividir o caminho em palavras
+    //Ex: C:/root/pedro/documentos 
+    //result: 'root','pedro','documentos';  
     parsePath(path) {
         if (path === '/') return ['/'];
         return path.split('/').filter(p => p !== '');
-      }
+    }
 
-    /*  TODO: Funções a fazer 
-    */
+    //achar diretório
+    findDir(path){
+        if(path === "/") {
+            this.currentDir = this.rootDir;
+        }
+        
+        let current = path.startsWith('/') ? th
+    }  
 }
 
 
